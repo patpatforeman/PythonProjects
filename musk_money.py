496911cf7e100cb7a48aba9d906ff1b085310092
patpatforeman <98058660+patpatforeman@users.noreply.github.com>
2022-01-24 10:29:37 -0500

    Create musk_money.py

---
+++ musk_money.py
@@ -0,0 +1,5 @@
+musk_investment = 1.02 # In Billion
+ten_year = musk_investment * 1.75
+twenty_year = musk_investment * 2.13
+print(str(round(ten_year, 2)) + ' Billion')
+print(str(round(twenty_year, 2))+ ' Billion')
