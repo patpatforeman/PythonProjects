1cc8a7f0c2d7a0ef2b612903d97f68c18bcbfb64
patpatforeman <98058660+patpatforeman@users.noreply.github.com>
2022-01-24 11:40:42 -0500

    Update musk_money.py

--- musk_money.py
+++ musk_money.py
@@ -1,5 +1,7 @@
-musk_investment = 1.02 # In Billion
-ten_year = musk_investment * 1.75
-twenty_year = musk_investment * 2.13
+musk_investment = 1.02 # in billion
+ten_year = musk_investment * (1 + (.0175))**10
+twenty_year = musk_investment * (1 +(0.0213))**20
 print(str(round(ten_year, 2)) + ' Billion')
 print(str(round(twenty_year, 2))+ ' Billion')
+yearly_income = (twenty_year - musk_investment)/ 20
+print(str(round(yearly_income, 2))+ ' Billion')
