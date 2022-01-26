a6b9eef0c4d9d111e0e408430685f9d52e2059d6
patpatforeman <patrickforeman123@gmail.com>
2022-01-25 21:02:00 -0500

    Minor print edits

--- musk_money.py
+++ musk_money.py
@@ -7,10 +7,14 @@ ten_year = musk_investment * (1 + (rate_tenner)) ** 10
 twenty_year = musk_investment * (1 + (rate_twenty)) ** 20
 
 # Print out Values at end of Period, in Billions
-print(str(round(ten_year, 2)) + ' Billion.')
-print(str(round(twenty_year, 2))+ ' Billion.')
+print('Elon Musk\'s initial investment is $' + str(musk_investment) + ' Billion.')
+
+print('If Musk puts the Tesla Sale money into a ten year bond, the value would increase to $' +
+      str(round(ten_year, 2)) + ' Billion.')
+print('If Musk puts the Tesla Sale money into a twenty year bond, the value would increase to $' +
+      str(round(twenty_year, 2)) + ' Billion.')
 
 # Twenty Year Investment for Musky
 yearly_income = (twenty_year - musk_investment) / 20
-print('The amount ole Musky would live on from interest on the twenty year investment is ' +
+print('The amount ole Musky would live on from interest on the twenty year investment is $' +
       str(round(yearly_income, 2)) + ' Billion.')
